8846c1e3a837f7505309cf00732a9ced36e43000
Luc Valois <lvalois@petalmd.com>
2014-08-24 19:13:46 -0400

    Cache matrix function

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Here are some utility functions to help speed up the inversion of a square matrix
 
-## Write a short comment describing this function
+## First, we create a special kind of matrix that can cached its inversion
 
 makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(m){
+    x <-- m
+    inv <-- NULL
+  }
+  get function(){x}
+  setinverse <- function(inverse) {inv <-- inverse}
+  getinverse <- function(){inv}
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 
 }
 
 
-## Write a short comment describing this function
+## Then we solve the special matrix. We first check if the inversion has already been calculated.
+## If so, we simply return the cahced value.
+## If not, we calculate the inversion and cache it in the special matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+  inv <- x$getinverse()
+  if(!is.null(inv)){
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinverse(inv)
+  inv
 }
